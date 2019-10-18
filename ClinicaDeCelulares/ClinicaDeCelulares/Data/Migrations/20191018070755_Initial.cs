8657351f7038ba220c5e22ca631017af1808d466
Jose Ignacio Pérez Matarrita <IgnacioPM@outlook.com>
2019-10-18 01:09:54 -0600

    Migracion

---
+++ ClinicaDeCelulares/ClinicaDeCelulares/Data/Migrations/20191018070755_Initial.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClinicaDeCelulares.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    IdCategoria = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    NombreCategoria = table.Column<string>(nullable: true),
+                    Descripcion = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.IdCategoria);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    IdCliente = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    NombreCliente = table.Column<string>(nullable: true),
+                    Direccion = table.Column<string>(nullable: true),
+                    TelefonoCliente = table.Column<string>(nullable: true),
+                    Fax = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.IdCliente);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Proveedores",
+                columns: table => new
+                {
+                    idProveedor = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    compania = table.Column<int>(nullable: false),
+                    nombreRepresentante = table.Column<int>(nullable: false),
+                    telefonoProveedor = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Proveedores", x => x.idProveedor);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    IdProducto = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    nombreProducto = table.Column<string>(nullable: true),
+                    precioUnidad = table.Column<decimal>(nullable: false),
+                    unidadesEnExistencia = table.Column<int>(nullable: false),
+                    idProveedor = table.Column<int>(nullable: false),
+                    idCategoria = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.IdProducto);
+                    table.ForeignKey(
+                        name: "FK_Productos_Categorias_idCategoria",
+                        column: x => x.idCategoria,
+                        principalTable: "Categorias",
+                        principalColumn: "IdCategoria",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Productos_Proveedores_idProveedor",
+                        column: x => x.idProveedor,
+                        principalTable: "Proveedores",
+                        principalColumn: "idProveedor",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Productos_idCategoria",
+                table: "Productos",
+                column: "idCategoria");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Productos_idProveedor",
+                table: "Productos",
+                column: "idProveedor");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+
+            migrationBuilder.DropTable(
+                name: "Proveedores");
+        }
+    }
+}

7387baa5a3a1c89473d4680b6af28ed53a6f82ce
Jose Ignacio Pérez Matarrita <IgnacioPM@outlook.com>
2019-10-18 22:44:56 -0600

    Migracion de cambio de atributos

--- ClinicaDeCelulares/ClinicaDeCelulares/Data/Migrations/20191019044344_cambio_atributos.Designer.cs
+++ ClinicaDeCelulares/ClinicaDeCelulares/Data/Migrations/20191019044344_cambio_atributos.Designer.cs
@@ -4,14 +4,16 @@ using ClinicaDeCelulares.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ClinicaDeCelulares.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191019044344_cambio_atributos")]
+    partial class cambio_atributos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

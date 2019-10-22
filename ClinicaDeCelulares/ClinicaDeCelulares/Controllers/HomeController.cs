7e140849a2b91048b45bb18db1107112642f3177
Jose Ignacio Pérez Matarrita <IgnacioPM@outlook.com>
2019-10-21 23:44:01 -0600

    Creacion de roles

--- ClinicaDeCelulares/ClinicaDeCelulares/Controllers/HomeController.cs
+++ ClinicaDeCelulares/ClinicaDeCelulares/Controllers/HomeController.cs
@@ -12,62 +12,50 @@ namespace ClinicaDeCelulares.Controllers
 {
     public class HomeController : Controller
     {
-        //public HomeController(IServiceProvider serviceProvider)
-        //{
-        //    CreateRoles(serviceProvider);
-        //}
-        public IActionResult Index()
+        IServiceProvider _serviceProvider;
+        public HomeController(IServiceProvider serviceProvider)
         {
+            _serviceProvider = serviceProvider;
+        }
+        public async Task<IActionResult> Index()
+        {
+            await CreateRoles(_serviceProvider);
             return View();
         }
-
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
             return View();
         }
-
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
-
             return View();
         }
-
         public IActionResult Privacy()
         {
             return View();
         }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
-        //private async Task CreateRoles(IServiceProvider serviceProvider)
-        //{
-        //    String mensaje;
-        //    try
-        //    {
-        //        var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-        //        var userManager = serviceProvider.GetRequiredService<RoleManager<IdentityUser>>();
-        //        string[] rolesName = { "Admin", "User" };
-        //        foreach (var item in rolesName)
-        //        {
-        //            var roleExist = await roleManager.RoleExistsAsync(item);
-        //            if (!roleExist)
-        //            {
-        //                await roleManager.CreateAsync(new IdentityRole(item));
-        //            }
-        //        }
-        //        var user = await userManager.FindByIdAsync("6b400e72 - cefd - 4335 - 9fdd - 31684e4a7791");
-        //        await userManager.AddToRoleAsync(user, "Admin");
-        //    }
-        //    cath(Exception ex){
-
-        //    }
-        //}
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            String[] rolesName = { "Admin", "Vende", "Tecni" };
+            foreach (var item in rolesName)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(item);
+                if (!roleExist)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(item));
+                }
+            }
+            var user = await userManager.FindByIdAsync("6b400e72-cefd-4335-9fdd-31684e4a7791");
+            await userManager.AddToRoleAsync(user, "Admin");
+        }
     }
 }

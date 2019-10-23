2a00521bf2c390155d87710bfeced7d6031fc50c
oscardf18 <56329182+oscardf18@users.noreply.github.com>
2019-10-22 22:10:02 -0600

    archivos css js y layaout

---
+++ ClinicaDeCelulares/ClinicaDeCelulares/wwwroot/js/Principal.js
@@ -0,0 +1,27 @@
+﻿
+class Principal {
+    constructor() {
+
+    }
+    userLink(URLactual) {
+        if (URLactual == "/Principal" || URLactual == "/Principal/") {
+            document.getElementById("enlace1").classList.add('active');
+        }
+        if (URLactual == "/Usuarios" || URLactual == "/Usuarios/" || URLactual == "/Usuarios/Index") {
+            document.getElementById("enlace2").classList.add('active');
+        }
+        if (URLactual == "/Usuarios/Eliminar/Eliminar" || URLactual == "/Usuarios/Eliminar/Eliminar/" ) {
+            document.getElementById("enlace2").classList.add('active');
+        }
+        if (URLactual == "/Usuarios/Registrar/Registrar" || URLactual == "/Usuarios/Registrar/Registrar/") {
+            document.getElementById("enlace2").classList.add('active');
+            var id = getParameterByName('id');
+            if (id != null) {
+                document.getElementById("Input_Role").selectedIndex = 1;
+                document.getElementById("imageRgistrar").innerHTML = ['<img class="responsive-img " src="', URL + "images/fotos/Usuarios/" + id + ".png", '" title="', escape(id), '"/>'].join('');
+                $("#divPassword").hide();
+            }
+        }
+       
+    }
+}
--- ClinicaDeCelulares/ClinicaDeCelulares/wwwroot/js/site.js
+++ ClinicaDeCelulares/ClinicaDeCelulares/wwwroot/js/site.js
@@ -1,4 +1,21 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿/*CODIGO PRINCIPAL*/
+var principal = new Principal();
 
-// Write your JavaScript code.
+
+/*CODIGO DE USUARIOS*/
+var usuarios = new Usuarios();
+var imageUser = (evt) => {
+    usuarios.archivo(evt, "imageRgistrar");
+}
+
+$().ready(() => {
+    let URLactual = window.location.pathname;
+    principal.userLink(URLactual);
+    //$('.sidenav').sidenav();
+    M.AutoInit();
+
+    if (URLactual == "/Usuarios/Registrar/Registrar" || URLactual == "/Usuarios/Registrar/Registrar/") {
+        document.getElementById('files').addEventListener('change', imageUser, false);
+        
+    }
+});
